6c25412de0f80558d6ff86bcd08c4447208e47ac
Beth1998 <tatyliz1998@gmail.com>
2024-11-06 13:18:27 -0500

    pag1y2

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Variables globales
+const botonPagina1 = document.getElementById('botonPagina1');
+const botonPagina2 = document.getElementById('botonPagina2');
+const botonRegresar = document.getElementById('botonRegresar');
+const botonRegresar2 = document.getElementById('botonRegresar2');
+const inicio = document.getElementById('inicio');
+const pagina1 = document.getElementById('pagina1');
+const pagina2 = document.getElementById('pagina2');
+const header1 = document.getElementById('.header1');
+const footer1 = document.getElementById('.footer1')
+const main1 = document.getElementById('main1')
+
+// Función para ir a una página específica
+function irAPagina(paginaInicio) {
+    // Ocultar todas las páginas
+    inicio.classList.add('oculta');
+    pagina1.classList.add('oculta');
+    pagina2.classList.add('oculta');
+    
+    
+    // Mostrar la página seleccionada
+    document.getElementById(paginaInicio).classList.remove('oculta');
+}
+
+// Función para regresar al inicio
+function regresarAlInicio() {
+    inicio.classList.remove('oculta');
+    pagina1.classList.add('oculta');
+    pagina2.classList.add('oculta');
+}
+
+// Asignamos los eventos de los botones
+botonPagina1.addEventListener('click', function() {
+    irAPagina('pagina1');
+});
+botonPagina2.addEventListener('click', function() {
+    irAPagina('pagina2');
+});
+botonRegresar.addEventListener('click', regresarAlInicio);
+botonRegresar2.addEventListener('click', regresarAlInicio);
